94c4d17af99ec64f201a69058a1e86696c009dcf
Matthew Ogram <emjayoh@gmail.com>
2016-11-16 15:32:53 -0800

    All file writes working, TODO: add extra nodes from missing properties

--- classes/AppFolio.php
+++ classes/AppFolio.php
@@ -11,7 +11,11 @@ class AppFolio {
   }
 
   private function getLastXMLModificationTime() {
-    return filemtime(RENTLINX_FILENAME);
+    if (file_exists(RENTLINX_FILENAME)) {
+      return filemtime(RENTLINX_FILENAME);
+    } else {
+      return null;
+    }
   }
 
   private function downloadPageIfModified() {
@@ -31,6 +35,10 @@ class AppFolio {
     $ret_val = curl_exec($ch);
     $local_modified = $this->getLastXMLModificationTime();
     $remote_modified = curl_getinfo($ch, CURLINFO_FILETIME);
+    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $ret_val_body = substr($ret_val, $header_size);
+    $this->xml_data = simplexml_load_string($ret_val_body);
+
 
 //    DEBUG
 //    echo 'local modfied: ' . $local_modified . '   ';
@@ -39,19 +47,56 @@ class AppFolio {
     // Only write file if not exist or older than remote
     if (!$local_modified || $local_modified < $remote_modified) {
       $modified = true;
-      file_put_contents(RENTLINX_FILENAME, $ret_val);
+      file_put_contents(RENTLINX_FILENAME, $ret_val_body);
     }
 
     curl_close($ch);
-    $this->raw_xml_data = simplexml_load_string($ret_val);
 
     return $modified;
   }
 
   private function callGoogleAPI() {
     foreach($this->xml_data->Properties->children() as $property) {
-      $full_address = $property->Address . ' ' . $property->City . ' ' . $property->State . ' ' . $properties->Zip;
+      $full_address = $property->Address . ' ' . $property->City . ' ' . $property->State . ' ' . $property->Zip;
+
+      if (isset($full_address) && !empty($full_address)) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, GOOGLE_GEOCODE_API_ENDPOINT . 'address=' . rawurlencode($full_address) . '&key=' . GOOGLE_API_KEY);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $ret_val = curl_exec($ch);
+
+        if (isset($ret_val) && !empty($ret_val)) {
+          $json = json_decode($ret_val);
+          var_dump($json->results[0]);
+
+          foreach($json->results[0]->address_components as $location) {
+            if (in_array('administrative_area_level_2', $location->types)) { $county = $location->long_name; };
+            if (in_array('neighborhood', $location->types)) { $neighborhood = $location->long_name; }
+            if (in_array('locality', $location->types)) { $city = $location->long_name; }
+
+            // Add Attributes to XML
+            if (!empty($city)) {
+              if ($city == 'Temecula') {
+                $county = 'Temecula';
+              } elseif ($city == 'Palm Springs') {
+                $county = 'Palm Springs';
+              }
+            }
+          }
+
+          $property->Address->addAttribute('full_address', $full_address);
+
+          if (isset($county) && !empty($county)) {
+            $property->Address->addAttribute('county', $county);
+          }
+
+          if (isset($neighborhood) && !empty($neighborhood)) {
+            $property->Address->addAttribute('neighborhood', $neighborhood);
+          }
+        }
+      }
     }
+    $this->xml_data->asXML('appfolio_1.xml');
   }
 
   private function printXMLData() {
